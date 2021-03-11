886c098f15ab9a7eb15597fc078d6441442fccb9
Lucas <64099992+lucas-r-leite@users.noreply.github.com>
2021-03-11 10:18:20 -0300

    Update Reaper install Shell

--- Reaper_install_Shell.sh
+++ Reaper_install_Shell.sh
@@ -1,11 +1,11 @@
 cd /home/$USER/Downloads/
 
 echo Instalando Reaper
-wget -c https://dlcf.reaper.fm/6.x/reaper618_linux_x86_64.tar.xz  #força os arquivos a serem instalados
+wget -c https://dlcf.reaper.fm/6.x/reaper625_linux_x86_64.tar.xz  #força os arquivos a serem instalados
 
 echo Descompactando o reaper
-tar -xvf reaper618_linux_x86_64.tar.xz
-rm reaper615_linux_x86_64.tar.xz
+tar -xvf reaper625_linux_x86_64.tar.xz
+rm reaper625_linux_x86_64.tar.xz
 cd reaper_linux_x86_64/
 ./install-reaper.sh
 
